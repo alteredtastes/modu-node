b60af48b8547a67d81f82274f01877dcb3d182ec
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-09-28 01:00:07 -0700

    saves new google user to mongo db with auth.google.login

--- routes/auth/google/login.js
+++ routes/auth/google/login.js
@@ -3,12 +3,6 @@ var needle = require('needle');
 var auth = require('../auth.subroutes');
 var User = require('../../../db/models/user/user');
 
-function login(req, res, next) {
-  needle.get(req.apiUrls[req.query.state], req.callOptions, function(err, resp) {
-    var googleData = resp.body;
-    console.log('google data ', googleData);
-    /*DATABASE*/
-
 /*SOCIAL ACCOUNT SCENARIOS
 LINKING ACCOUNTS: Check if a user already exists in the database. If they do, then add a social account to their user profile
 ACCOUNT CREATION: If a user does not exist in the database, then create a user profile
@@ -16,14 +10,19 @@ UNLINKING: Unlinking social accounts.
 RECONNECTING: If a user unlinked a social account, but wants to reconnect it
 */
 
-  /*IF USER IS CURRENTLY LOGGED IN, LINK THEIR GOOGLE ACCOUNT*/
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-  if(token) linkGoogle(token);
+function login(req, res, next) {
+  needle.get(req.apiUrls[req.query.state], req.callOptions, function(err, resp) {
+    var googleData = resp.body;
+    console.log(googleData);
 
-  /*IF USER DOES NOT EXIST IN DB, CREATE A PROFILE*/
-  googleCreateProfile(googleData, token);
+    /*IF USER IS CURRENTLY LOGGED IN, LINK THEIR GOOGLE ACCOUNT*/
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if(token) linkGoogle(googleData, token);
 
-  /*IF USER WANTS TO UNLINK ACCOUNT --> HAPPENS IN UNLINK ROUTE*/
+    /*IF USER DOES NOT EXIST IN DB, CREATE A PROFILE*/
+    googleCreateProfile(googleData, token);
+
+    /*IF USER WANTS TO UNLINK ACCOUNT --> HAPPENS IN UNLINK ROUTE*/
 
     var gmail = googleData.emails[0].value
     var user = gmail.substring(0, gmail.indexOf('@'));
@@ -41,22 +40,21 @@ RECONNECTING: If a user unlinked a social account, but wants to reconnect it
   });
 }
 
-function linkGoogle(token) {
+function linkGoogle(googleData, token) {
   jwt.verify(token, process.env.APP_SECRET), function(err, decoded) {
     if(err) {
       console.log('TOKEN ERROR at auth.google.login');
       res.redirect('/auth/login');
     } else {
       id = decoded.id;
-      User.findOne({ local.id : id }, function(err, user) {
+      User.findOne({ 'local.id' : id }, function(err, user) {
         if(err) console.log(err);
         user.google.token = req.apiPost.refresh_token;
         user.google.id  = googleData.id;
-        user.google.email = googleData.emails.value[0];
+        user.google.email = googleData.emails[0].value;
         user.google.displayName = googleData.displayName;
         user.save(function(err) {
           if (err) console.log(err);
-          res.redirect('/');
         });
       });
     }
@@ -67,9 +65,12 @@ function googleCreateProfile(googleData, token) {
   var newUser = new User();
   newUser.google.id = googleData.id;
   newUser.google.token = token;
-  newUser.google.email = googleData.emails.value[0];
+  newUser.google.email = googleData.emails[0].value;
   newUser.google.displayName = googleData.displayName;
-  res.redirect('/');
+  // newUser.local.id = new id number;
+  newUser.save(function(err) {
+    if(err) console.log(err);
+  });
 }
 
 module.exports = login;
